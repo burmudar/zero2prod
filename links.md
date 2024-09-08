396a77fc37b97a045e2551e84cf84379bd10bf8a
William Bezuidenhout <william.bezuidenhout@gmail.com>
2024-09-08 21:45:33 +0200

    healthcheck endpoint and test

---
+++ links.md
@@ -0,0 +1,6 @@
+# Useful links
+
+- [actix-web](https://actix.rs/)
+- [actix-docs](https://actix.rs/docs)
+  - [Rust API](https://docs.rs/actix-web/latest/actix_web/)
+- [actix-exmaples](https://github.com/actix/actix/tree/master/actix/examples)
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{web, App, HttpResponse, HttpServer};
+
+async fn health_check() -> HttpResponse {
+    HttpResponse::Ok().finish()
+}
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
+        .bind("127.0.0.1:8000")?
+        .run()
+        .await
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::health_check;
+
+    #[tokio::test]
+    async fn health_check_succeeds() {
+        let response = health_check().await;
+
+        assert!(response.status().is_success())
+    }
 }
