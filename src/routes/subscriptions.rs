04d047ad454bbf3c9650e8c983565a55ea2f57e8
William Bezuidenhout <william.bezuidenhout@gmail.com>
2024-10-01 21:01:44 +0200

    use tracing-actix-web
    remove manually specifying request_id since we get it now from using
    TraceLogger

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -13,7 +13,6 @@ pub struct FormData {
     name = "adding new subscriber",
     skip(form, pool),
     fields(
-        request_id = %Uuid::new_v4(),
         // the '%' below tells tracing to use their Display trait when rendering their values
         subscriber_email = %form.email,
         subscriber_name = %form.name,
